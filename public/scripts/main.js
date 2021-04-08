f34acb749122c163ab4824203f0db2b9374ce7a5
Megan Morris <megs@Mac-mini.local>
2021-04-08 10:59:15 -0400

    .

---
+++ public/scripts/main.js
@@ -0,0 +1,36 @@
+
+
+(() => {
+    //storing a handle for the subtitles container and controls 
+    const videoContainer = document.getElementById('videoContainer');
+    const subtitles = document.getElementById('subtitles');
+    const videoControls = document.getElementById('video-controls');
+
+const playpause = document.getElementById('playpause');
+const stop = document.getElementById('stop');
+const mute = document.getElementById('mute');
+const volinc = document.getElementById('volinc');
+const voldec = document.getElementById('voldec');
+const progress = document.getElementById('progress');
+const progressBar = document.getElementById('progress-bar');
+const fullscreen = document.getElementById('fs');
+
+//setting up the event listeners for each button
+playpause.addEventListener('click', function(e) {
+    if (video.paused || video.ended) video.play();
+    else video.pause();
+ });
+ stop.addEventListener('click', function(e) {
+    video.pause();
+    video.currentTime = 0;
+    progress.value = 0;
+ });
+ mute.addEventListener('click', function(e) {
+    video.muted = !video.muted;
+ });
+ subtitles.addEventListener('click', function(e) {
+    if (subtitlesMenu) {
+       subtitlesMenu.style.display = (subtitlesMenu.style.display == 'block' ? 'none' : 'block');
+    }
+ });
+})();
